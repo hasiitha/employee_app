ba50a8b37396a2d16d23fa68c4af67f56c381651
lochana-vishwajith <lochanavishwajith302@gmail.com>
2022-11-17 13:55:59 +0530

    promise added

--- SSD_Backend-master/service/userService.js
+++ SSD_Backend-master/service/userService.js
@@ -3,51 +3,56 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 const userLoging = async (username, password) => {
-  const user = await User.findOne({ username });
+  console.log("inside service");
+  return new Promise(async (resolve, reject) => {
+    const user = await User.findOne({ username });
 
-  if (user && (await bcrypt.compare(password, user.password))) {
-    return {
-      status: 200,
-      obj: {
-        _id: user._id,
-        username: user.username,
-        token: generateToken(user._id, user.role),
-        role: user.role,
-      },
-    };
-  } else {
-    return { status: 400, obj: "invalid" };
-  }
+    if (user && (await bcrypt.compare(password, user.password))) {
+      resolve({
+        status: 200,
+        obj: {
+          _id: user._id,
+          username: user.username,
+          token: generateToken(user._id, user.role),
+          role: user.role,
+        },
+      });
+    } else {
+      reject({ status: 400, obj: "invalid" });
+    }
+  });
 };
 
 const UserRegistration = async (username, password, role) => {
-  const isAvailable = await User.findOne({ username });
+  return new Promise(async (resolve, reject) => {
+    const isAvailable = await User.findOne({ username });
 
-  if (isAvailable) {
-    return { status: 400, obj: "invalid" };
-  } else {
-    const saltedVal = await bcrypt.genSalt(10);
-    const hashedPW = await bcrypt.hash(password, saltedVal);
-
-    const userDetails = await User.create({
-      username,
-      password: hashedPW,
-      role,
-    });
-
-    if (userDetails) {
-      return {
-        status: 201,
-        obj: {
-          _id: userDetails._id,
-          username: userDetails.username,
-          role: userDetails.role,
-        },
-      };
+    if (isAvailable) {
+      return { status: 400, obj: "invalid" };
     } else {
-      return { status: 400 };
+      const saltedVal = await bcrypt.genSalt(10);
+      const hashedPW = await bcrypt.hash(password, saltedVal);
+
+      const userDetails = await User.create({
+        username,
+        password: hashedPW,
+        role,
+      });
+
+      if (userDetails) {
+        resolve({
+          status: 201,
+          obj: {
+            _id: userDetails._id,
+            username: userDetails.username,
+            role: userDetails.role,
+          },
+        });
+      } else {
+        reject({ status: 400 });
+      }
     }
-  }
+  });
 };
 
 const generateToken = (id, role) => {
---
+++ SSD_Backend-master/test/message.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const app = require("../server");
+const { HttpStatusCode } = require("axios");
+
+describe("Add new message", () => {
+  it("Check authenitcation of add new message end point", async () => {
+    const res = await request(app)
+      .post("/messages/messages")
+      .set({ token: "In valid token", Accept: "application/json" })
+      .send({
+        message: "hi",
+        sender: "5f38c4f9eb0ca06da4102b58de6a924e",
+        encryptedMsg: "a533b2d31573847331ba2af55f7fe9c0",
+      });
+    expect(res.statusCode).toEqual(400);
+  });
+});
+
+describe("POST /api/products/:id", () => {
+  it("should update a product", async () => {
+    const res = await request(app).post("/messages/messages").send({
+      message: "hi",
+      sender: "5f38c4f9eb0ca06da4102b58de6a924e",
+      encryptedMsg: "a533b2d31573847331ba2af55f7fe9c0",
+    });
+    expect(res.statusCode).toBe(201);
+  });
+});
